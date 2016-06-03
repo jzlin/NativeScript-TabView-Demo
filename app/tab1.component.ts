c59e7e355e3c0474dfe53cbdbb58101e306f9ece
Jerry <jerry@miniasp.com>
2016-06-03 11:11:49 +0800

    Using Page Router Outlet.

--- app/tab1.component.ts
+++ app/tab1.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { RouteConfig } from '@angular/router-deprecated';
+import { NS_ROUTER_PROVIDERS, NS_ROUTER_DIRECTIVES } from 'nativescript-angular/router';
 
 import { ItemListComponent } from './item-list.component';
 
 @Component({
     moduleId: module.id,
+    providers: [
+        NS_ROUTER_PROVIDERS
+    ],
     directives: [
-        ItemListComponent
+        NS_ROUTER_DIRECTIVES
     ],
     selector: 'tab1',
-    template: `<item-list></item-list>`
+    template: `<page-router-outlet></page-router-outlet>`
 })
+@RouteConfig([
+    { path: "/", name: "ItemList", component: ItemListComponent }
+])
 export class Tab1Component implements OnInit {
     constructor() { }
 
