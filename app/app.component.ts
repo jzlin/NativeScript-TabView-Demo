8d2678c2bb517129c0935219a86781d491a60860
Jerry <jerry@miniasp.com>
2016-06-03 10:37:06 +0800

    Create TabView.

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,27 +3,14 @@ import {Component} from "@angular/core";
 @Component({
     selector: "my-app",
     template: `
-<StackLayout>
-    <Label text="Tap the button" class="title"></Label>
-    
-    <Button text="TAP" (tap)="onTap()"></Button>
-
-    <Label [text]="message" class="message" textWrap="true"></Label>
-</StackLayout>
+<TabView>
+    <StackLayout *tabItem="{title: 'Tab1'}">
+        <Label text="tab1 content"></Label>
+    </StackLayout>
+    <StackLayout *tabItem="{title: 'Tab2'}">
+        <Label text="tab2 content"></Label>
+    </StackLayout>
+</TabView>
 `,
 })
-export class AppComponent {
-    public counter: number = 16;
-
-    public get message(): string {
-        if (this.counter > 0) {
-            return this.counter + " taps left";
-        } else {
-            return "Hoorraaay! \nYou are ready to start building!";
-        }
-    }
-    
-    public onTap() {
-        this.counter--;
-    }
-}
+export class AppComponent {}
